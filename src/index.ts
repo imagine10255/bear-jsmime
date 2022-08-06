6114e3c54e43377057de81285fc96b67037a4063
imagine10255 <imagine10255@gmail.com>
2022-08-06 16:56:28 +0800

    fix package

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export * from './bear-jsmine';
+import {mineTypeMap} from './db';
+
+export const mineType2extname = (mineType: string): string => {
+    const findExtname = mineTypeMap[mineType];
+    if(findExtname?.length > 0){
+        return findExtname[0];
+    }
+    return '';
+};
