e62fad775834d45c20a5de4aeabd35b44d2719c6
imagine10255 <imagine10255@gmail.com>
2023-03-13 00:29:56 +0800

    fix: fix extname2mineType replace . and modify use includes

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,10 @@ export const mineType2extname = (mineType: string): string => {
     return '';
 };
 export const extname2mineType = (extname: string): string => {
+    const formatExtname = extname.replace('.','');
     const parse = Object.entries(mineTypeMap);
     const active = parse.find(row => {
-        return row[1].find(extnameRow => extnameRow === extname);
+        return row[1].includes(formatExtname);
     });
     if(active){
         return active[0];
